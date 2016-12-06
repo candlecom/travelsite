1b71acd5492408ebae6b24da8215dbdbc145de41
Bruce <brucev@theedge.ca>
2016-12-06 16:36:32 -0700

    git cache cleared

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,19 @@ nested = require('postcss-nested'),
 cssImport = require('postcss-import');
 
 gulp.task('default', function() {
-  console.log("Hooray - you created a Gulp task.");
+  console.log("Hooray");
 });
 
+
 gulp.task('html', function() {
-  console.log("Imagine something useful being done to your HTML here.");
+  console.log("Imagine");
 });
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    //.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
+
 });
 
 gulp.task('watch', function() {
