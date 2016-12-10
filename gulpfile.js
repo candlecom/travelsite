b81a45ca85eab654985038051e173d91f417172a
Bruce <brucev@theedge.ca>
2016-12-09 22:12:21 -0700

    Commit before git pull

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', function() {
-  console.log("Hooray");
+  console.log("Hooray - you created a Gulp task.");
 });
 
 gulp.task('html', function() {
-  console.log("Imagine");
+  console.log("Imagine something useful being done to your HTML here.");
 });
 
 gulp.task('styles', function() {
-  console.log("Imagine SASS");
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
@@ -20,7 +26,7 @@ gulp.task('watch', function() {
   });
 
   watch('./app/assets/styles/**/*.css', function() {
-      gulp.start('styles');
-    });
+    gulp.start('styles');
+  });
 
 });
